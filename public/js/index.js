27229506dcef7275aa44d18594858c518bfffaac
JamesTGH <26239426+JamesTGH@users.noreply.github.com>
2023-12-10 22:32:14 -0600

    Updated index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,37 @@
+function getPlaybackState(refreshInterval) {
+	if (ws.readyState === WebSocket.OPEN) ws.send(JSON.stringify({ type: 'get-playback-state' }));
+	setTimeout(() => {
+		getPlaybackState(refreshInterval);
+	}, refreshInterval);
+}
+
 $(document).ready(function() {
 	function connectWebSocket() {
-		const ws = new WebSocket(`wss://${window.location.hostname}`);
+		ws = new WebSocket(`wss://${window.location.hostname}`);
 
 		ws.addEventListener('open', (event) => {
 			console.log('WS Connected!');
-			setInterval(() => {
-				ws.send(JSON.stringify({ type: 'get-playback-state' }));
-			}, 4e3);
+			getPlaybackState(4e3);
 		});
 
 		ws.addEventListener('message', (event) => {
 			const response = JSON.parse(event.data);
 			switch (response.type) {
-			case 'get-playback-state':
-				// console.log(response.data);
-				$('#album-href').attr('href', response.data.item.album.external_urls.spotify);
-				$('#album-image').attr('src', response.data.item.album.images[1].url);
-				$('#track-href').attr('href', response.data.item.external_urls.spotify);
-				$('#track-name').text(response.data.item.name);
-				let artists = [];
-				for(const artist of response.data.item.artists) {
-					artists.push(artist.name);
-				}
-				$('#track-artists').text(artists.join(', '));
-				break;
-			case 'get-track':
-				console.log(response.data);
-				break;
+				case 'get-playback-state':
+					console.log(response.data);
+					$('#album-href').attr('href', response.data.item.album.external_urls.spotify);
+					$('#album-image').attr('src', response.data.item.album.images[1].url);
+					$('#track-href').attr('href', response.data.item.external_urls.spotify);
+					$('#track-name').text(response.data.item.name);
+					let artists = [];
+					for(const artist of response.data.item.artists) {
+						artists.push(artist.name);
+					}
+					$('#track-artists').text(artists.join(', '));
+					break;
+				case 'get-track':
+					console.log(response.data);
+					break;
 			}
 		});
 
